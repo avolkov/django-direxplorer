b677dd08a4a4b9249e28c449b941ff0432971f01
Alex Volkov <alex@flamy.ca>
2011-08-16 22:23:48 -0400

    Got most of boiler plate done.

--- explorer/filesystem_explorer.py
+++ explorer/filesystem_explorer.py
@@ -1,4 +1,39 @@
+import os
+from pprint import pprint as pp
+from collections import OrderedDict
 
+#def traverse_path(path):
+#    os.walk(path)
 
-def hw():
-    return "Hello world!"
+mypath = '/home/alex/tmp'
+
+def listdir(): 
+    
+    everything = os.listdir(mypath)  
+    
+    files = [k for k in everything if os.path.isfile(os.path.join(mypath, k)) ]
+    directories = [k for k in everything if os.path.isdir(os.path.join(mypath, k)) ]
+    
+    files.sort()
+    directories.sort()
+    
+    file_dict = OrderedDict([(x, ['file', os.path.getsize(os.path.join(mypath,x))]) for x in files])
+    dir_dict = OrderedDict([(x, ['dir'])for x in directories])
+    
+    #import pdb; pdb.set_trace()   
+    return (dir_dict, file_dict)
+
+def directory_walker(directory):
+    '''
+    for k in os.listdir(mypath):
+        fullpath = os.path.join(mypath, k)
+        if os.path.isfile(fullpath):
+            entry[k] = [ 'file' ]
+            entry[k].append(os.path.getsize(fullpath))
+        elif os.path.isdir(fullpath):
+            entry[k] = [ 'dir' ]
+    #import pdb; pdb.set_trace()
+    '''
+    #for k in os.walk(mypath):
+    #    print k
+    #    import pdb; pdb.set_trace()
--- explorer/views.py
+++ explorer/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from filesystem_explorer import hw as hw1
+from django.template import RequestContext, loader
+from django.template import Context, Template
+from filesystem_explorer import listdir
+
 
 def hw(request):
-    return HttpResponse(hw1())
+    #template = Template('templates/base.html')
+    #template = Template("Hello, world!")
+    #ctx = Context({'listing':listdir()})
+    #return template.render(ctx)
+    
+    return render_to_response('base.html', {'data': listdir()}, context_instance=RequestContext(request))
