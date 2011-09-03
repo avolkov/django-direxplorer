f19ff01c19c5ac47218bf31b49de36dbcfa3224a
Alex Volkov <avolkov@gmail.com>
2011-09-03 17:50:45 -0400

    Minor cleanup

--- explorer/views.py
+++ explorer/views.py
@@ -3,17 +3,13 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.template import Context, Template
-from filesystem_explorer import listdir, root_path, fe_mime
+from filesystem_explorer import listdir, root_path, fe_mime, arc_zip
 import os.path
 
 
 
 
 def hw(request):
-    #template = Template('templates/base.html')
-    #template = Template("Hello, world!")
-    #ctx = Context({'listing':listdir()})
-    #return template.render(ctx)
     return render_to_response('base.html', {'data': listdir(root_path)}, context_instance=RequestContext(request))
 
 def zip(request):
@@ -24,13 +20,10 @@ def zip(request):
 def raw(request):
     url_path = "/".join(request.META['PATH_INFO'].split('/')[2:-1])
     fullpath = os.path.join(root_path, url_path)
-    import ipdb; ipdb.set_trace()
     response = HttpResponse(open(fullpath, 'r'),mimetype=fe_mime.guess_type(url_path)[0])
     response['content-Disposition'] = "attachment; filename=%s" % fullpath.split('/')[-1]
     response['Content-Length'] = os.path.getsize(fullpath)
     return response
 def explore(request):
     url_path = "/".join(request.META['PATH_INFO'].split('/')[2:])
-    #import ipdb; ipdb.set_trace()
-    #return HttpResponse("Exploring: %s" % file_path)
     return render_to_response('base.html', {'data': listdir(root_path, url_path)}, context_instance=RequestContext(request))
