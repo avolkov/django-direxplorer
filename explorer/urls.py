041085a67dc1d080acf7963faaa79c5c922b9ca5
Alex Volkov <avolkov@gmail.com>
2011-09-02 23:54:15 -0400

    Added file serving view.

--- explorer/urls.py
+++ explorer/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^file_explorer/', include('file_explorer.foo.urls')),
-    (r'^$','explorer.views.hw')
+    (r'^$','explorer.views.hw'),
+    (r'^[a-zA-Z0-9/-_]+/zip$', 'explorer.views.zip'),
     #(r'^explorer/', include('file_explorer.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- explorer/views.py
+++ explorer/views.py
@@ -13,3 +13,11 @@ def hw(request):
     #return template.render(ctx)
     
     return render_to_response('base.html', {'data': listdir()}, context_instance=RequestContext(request))
+def zip(request):
+    response = HttpResponse(mimetype='application/zip')
+    response['content-Disposition'] = "attachment; filename=%s.zip" % request.path.split("/")[-2]
+    response.write('0'*50)
+    return response
+    #import pdb; pdb.set_trace()
+    #return HttpResponse("Returning zipped data")
+    #return render_to_response("Returning zipped data")
