9b7571e85a001e75319a3786f63dc8ddec6cfd25
Alex Volkov <alex@flamy.ca>
2011-09-03 23:24:49 -0400

    Minor tweaks in urls.py

--- explorer/urls.py
+++ explorer/urls.py
@@ -5,7 +5,7 @@ file_pattern = "[a-zA-Z0-9/-_.]+"
 urlpatterns = patterns('',
     # Example:
     # (r'^file_explorer/', include('file_explorer.foo.urls')),
-    (r'^$','explorer.views.hw'),
+    (r'^$','explorer.views.explore'),
     (r'^%s/zip$' % file_pattern, 'explorer.views.zip'),
     (r'^%s/raw$' % file_pattern, 'explorer.views.raw'),
     (r'^%s$' % file_pattern, 'explorer.views.explore'),
