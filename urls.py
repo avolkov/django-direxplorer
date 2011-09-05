53032f3e4ef56304a905eba85b1f5c77f6a61de3
Alex Volkov <alex@flamy.ca>
2011-09-05 14:39:10 -0400

    Updated url references.

--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.defaults import *
 
 file_pattern = "[a-zA-Z0-9/-_.]+"
 
-urlpatterns = patterns('',
-    (r'^$','explorer.views.explore'),
-    (r'^%s/zip$' % file_pattern, 'explorer.views.zip'),
-    (r'^%s/raw$' % file_pattern, 'explorer.views.raw'),
-    (r'^%s$' % file_pattern, 'explorer.views.explore'),
+urlpatterns = patterns('django-direxplorer.views',
+    (r'^$','explore'),
+    (r'^%s/zip$' % file_pattern, 'zip'),
+    (r'^%s/raw$' % file_pattern, 'raw'),
+    (r'^%s$' % file_pattern, 'explore'),
 )
